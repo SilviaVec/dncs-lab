335b23eff8b6c69bf31a9d2a17bfbadf4ab7e8c4
SilviaVec <44430562+SilviaVec@users.noreply.github.com>
2019-03-24 09:28:31 +0100

    Add files via upload

--- switch.sh
+++ switch.sh
@@ -2,3 +2,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump --assume-yes
 apt-get install -y openvswitch-common openvswitch-switch apt-transport-https ca-certificates curl software-properties-common
+sudo su
+ovs-vsctl --if-exists del-br switch
+ovs-vsctl add-br switch
+ovs-vsctl add-port switch eth1
+ovs-vsctl add-port switch eth2 tag=1
+ovs-vsctl add-port switch eth3 tag=2
+ip link set dev eth1 up
+ip link set dev eth2 up
+ip link set dev eth3 up
+ip link set ovs-system up
